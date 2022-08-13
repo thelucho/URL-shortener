9feaa21790e7b1ed33215f21b08ae8fc442784a5
Luciano Dichiara <luciano@studiohakuna.com>
2022-08-13 16:08:09 -0300

    Some UI adjustments

--- src/components/ShortenerBox.vue
+++ src/components/ShortenerBox.vue
@@ -10,25 +10,31 @@
 
 <template>
   <div class="bg-white rounded-2xl border shadow-x1 p-10 max-w-lg">
-        <div class="flex flex-col items-center space-y-4">
-          <h1 class="font-bold text-2xl text-gray-700 w-4/6 text-center">
-            URL Shortener
-          </h1>
-          <p class="text-sm text-gray-500 text-center w-5/6">
-            Hello, please enter the URL you want to shorten.
-          </p>
-          <input
-            type="url"
-            placeholder="Paste your long URL..."
-            class="border-2 rounded-lg w-full h-12 px-4"
-          />
-          <button
-            class="bg-red-400 text-white rounded-md hover:bg-red-500 font-semibold px-4 py-3 w-full"
-          >
-            Make your magic!
-          </button>
-        </div>
-      </div>
+    <div class="flex flex-col items-center space-y-4">
+      <h1 class="font-bold text-2xl text-gray-700 w-5/6 text-center">
+        URL Shortener 🎯
+      </h1>
+      <p class="text-sm text-gray-500 text-center w-6/6">
+        Hello, are you ready to shorten your URLs?
+      </p>
+      <input
+        type="url"
+        placeholder="Paste your looooong URL..."
+        class="border-2 rounded-lg w-full h-12 px-4"
+      />
+      <button
+        class="bg-emerald-400 text-white rounded-md hover:bg-emerald-500 font-semibold px-4 py-3 w-full"
+      >
+        Do the magic!
+      </button>
+    </div>
+  </div>
+  <div class="py-10 px-5 text-sm">
+    Made with
+    <span class="py-0.5 px-2 ml-1 border rounded">Vite</span>
+    <span class="py-0.5 px-2 ml-2 border rounded">Vue 3</span>
+    <span class="py-0.5 px-2 ml-2 border rounded">Tailwind</span>
+  </div>
 </template>
 
 <style scoped>

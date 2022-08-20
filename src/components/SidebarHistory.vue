02df0b24edec2feb177006875b55490a79ca9b75
Luciano Dichiara <luciano@studiohakuna.com>
2022-08-20 17:47:58 -0300

    Add Heroicons

--- src/components/SidebarHistory.vue
+++ src/components/SidebarHistory.vue
@@ -6,29 +6,62 @@
       <h2 class="text-lg font-semibold mb-6">Shortened URLs history</h2>
       <ul>
         <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
-          <span class="border-l-4 border-indigo-400 mb-2 pl-3 py-1 text-slate-300">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
-          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium">https://bit.ly/a345sdfnsdf</span>
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://dev.to/cwraytech/triggering-a-deploy-in-netlify-automatically-when-updating-content-in-strapi-28c0</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
         </li>
         <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
-          <span class="border-l-4 border-indigo-400 mb-2 pl-3 py-1 text-slate-300">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
-          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium">https://bit.ly/a345sdfnsdf</span>
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
         </li>
         <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
-          <span class="border-l-4 border-indigo-400 mb-2 pl-3 py-1 text-slate-300">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
-          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium">https://bit.ly/a345sdfnsdf</span>
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://dev.to/cwraytech/triggering-a-deploy-in-netlify-automatically-when-updating-content-in-strapi-28c0</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
         </li>
         <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
-          <span class="border-l-4 border-indigo-400 mb-2 pl-3 py-1 text-slate-300">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
-          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium">https://bit.ly/a345sdfnsdf</span>
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
         </li>
         <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
-          <span class="border-l-4 border-indigo-400 mb-2 pl-3 py-1 text-slate-300">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
-          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium">https://bit.ly/a345sdfnsdf</span>
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://dev.to/cwraytech/triggering-a-deploy-in-netlify-automatically-when-updating-content-in-strapi-28c0</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
+        </li>
+        <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://www.codegrepper.com/code-examples/javascript/how+to+post+form+data+using+axios</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
+        </li>
+        <li class="py-2 border-b border-teal-lighter text-left flex flex-col text-sm">
+          <span class="border-l-4 border-gray-300 mb-2 pl-3 py-1 text-slate-400">https://dev.to/cwraytech/triggering-a-deploy-in-netlify-automatically-when-updating-content-in-strapi-28c0</span>
+          <span class="border-l-4 border-green-400 pl-3 py-1 font-medium flex align-center">
+            https://bit.ly/a345sdfnsdf
+            <ClipboardCheckIcon class="ml-2 h-5 w-5 text-slate-300 hover:text-slate-400 cursor-pointer" />
+          </span>
         </li>
       </ul>
     </div>
   </section>
 </template>
+
+<script setup>
+import { ClipboardCheckIcon } from '@heroicons/vue/outline'
+</script>
 
 <style scoped>
 .sidebar {
